41584c9474e8e114e0a08cd28c861504f9d937f6
Ivan López <ivan.lopez1@unmsm.edu.pe>
2022-02-25 10:48:47 -0500

    creando website module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './website/pages/home/home.component';
-import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { MyCartComponent } from './website/pages/my-cart/my-cart.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { RegisterComponent } from './website/pages/register/register.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { ProfileComponent } from './website/pages/profile/profile.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
-import { LayoutComponent } from './website/components/layout/layout.component';
+
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
@@ -17,46 +9,7 @@ const routes: Routes = [
 
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch : 'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'category/:id',
-        component: CategoryComponent
-      },
-      {
-        path: 'my-cart',
-        component: MyCartComponent
-      },
-      {
-        path: 'login',
-        component: LoginComponent
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'recovery',
-        component: RecoveryComponent
-      },
-      {
-        path: 'profile',
-        component: ProfileComponent
-      },
-      {
-        path: 'product/:id',
-        component: ProductDetailComponent
-      },
-    ]
+    loadChildren: () => import ('./website/website.module').then(m => m.WebsiteModule)
   },
   {
     path: 'cms',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import { SwiperModule } from 'swiper/angular';
 // import SwiperCore, { Pagination } from "swiper";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImgComponent } from './website/components/img/img.component';
 import { FormsModule } from '@angular/forms';
-import { ProductComponent } from './website/components/product/product.component';
-import { ListProductsComponent } from './website/components/list-products/list-products.component';
-import { NavComponent } from './website/components/nav/nav.component';
-import { ReversePipe } from './website/pipes/reverse.pipe';
-import { TimeAgoPipe } from './website/pipes/time-ago.pipe';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { TokenInterceptor } from './interceptors/token.interceptor';
-import { HomeComponent } from './website/pages/home/home.component';
-import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { MyCartComponent } from './website/pages/my-cart/my-cart.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { RegisterComponent } from './website/pages/register/register.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { ProfileComponent } from './website/pages/profile/profile.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
-import { LayoutComponent } from './website/components/layout/layout.component';
 
 //SwiperCore.use([Pagination]);
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent,
-    ProductComponent,
-    ListProductsComponent,
-    NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HomeComponent,
-    NotFoundComponent,
-    CategoryComponent,
-    MyCartComponent,
-    LoginComponent,
-    RegisterComponent,
-    RecoveryComponent,
-    ProfileComponent,
-    ProductDetailComponent,
-    LayoutComponent
+    NotFoundComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    SwiperModule
+
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi:true}
